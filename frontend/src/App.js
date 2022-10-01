8dc65f39f424600d009872dffc2e2bcca7736887
MarkCodering <mark.chen.sstm@gmail.com>
2022-10-01 11:08:42 +0800

    initial frontend set-up and configuration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Input, Text } from '@nextui-org/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-background">
+        <Grid.Container justify="center" alignItems="center" direction="column">
+          <Grid justify="center" alignItems="center" direction="column">
+            <Text h1>Enter your Search</Text>
+          </Grid>
+          <Grid justify="center" alignItems="center" direction="column">
+            <Input width="350px" animated placeholder="Your Idea..." />
+          </Grid>
+        </Grid.Container>
+
+      </div>
     </div>
   );
 }
